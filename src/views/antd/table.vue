ea7456b739758e65e0a2a553632a9de49999ac47
Dittonal <53260392+Dittonal@users.noreply.github.com>
2022-11-16 16:51:01 +0800

    antd表格请求数据库

--- src/views/antd/table.vue
+++ src/views/antd/table.vue
@@ -1,12 +1,45 @@
 <template>
-  <h1>Antd内容</h1>
+  <h1>Antd表格</h1>
+  <a-table
+  :columns="columns"
+  :data-source="data"
+></a-table>
 </template>
 
 <script>
+import axios from "axios";
+const request = axios.create();
 export default {
   data() {
-    return {};
+    return {
+      data:[],
+      columns:[
+        {
+        title: '名字',  //标题名字
+        dataIndex: 'name',
+        },
+        {
+        title: '价格',  //标题名字
+        dataIndex: 'price',
+        },
+        {
+        title: '数量',  //标题名字
+        dataIndex: 'number',
+        },
+        ]
+    };
   },
+  mounted() {
+    this.getData();
+  },
+  methods:{
+    getData(){
+      request.get('https://my-json-server.typicode.com/Dittonal/testapi/db')
+      .then((res)=>{
+        this.data=res.data['goods']
+      })
+    },
+  }
 };
 </script>
 
